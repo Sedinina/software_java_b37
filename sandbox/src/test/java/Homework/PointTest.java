24b10df0c23e028611500d9de2db7544b527653b
Sedinina <sedinina.k@yandex.ru>
2023-02-27 20:50:10 +0500

    Задание 3 - попытка 2

--- sandbox/src/test/java/Homework/PointTest.java
+++ sandbox/src/test/java/Homework/PointTest.java
@@ -10,6 +10,26 @@ public class PointTest {
         Point p1 = new Point(1,3);
         Point p2 = new Point(2,4);
         Assert.assertEquals( p1.distance(p2), 1.4142135623730951);
-
     }
+    @Test
+    public void testPoint1() {
+        Point p1 = new Point(1,3);
+        Point p2 = new Point(2,4);
+       Assert.assertEquals( p1.distance(p2), 1.41);
+    }
+
+    @Test
+    public void testPoint2() {
+        Point p1 = new Point(1,3);
+        Point p2 = new Point(2,4);
+        Assert.assertEquals( p1.distance(p2), 0);
+    }
+
+    @Test
+    public void testPoint3() {
+        Point p1 = new Point(1,3);
+        Point p2 = new Point(2,4);
+        Assert.assertEquals( p1.distance(p2), -1.4142135623730951);
+    }
+
 }
