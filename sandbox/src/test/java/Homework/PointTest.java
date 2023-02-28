c8ba6025ef2ebfee74d9f7f0349da3a7a233e4d1
Sedinina <sedinina.k@yandex.ru>
2023-02-28 19:58:24 +0500

    исправления №3

--- sandbox/src/test/java/Homework/PointTest.java
+++ sandbox/src/test/java/Homework/PointTest.java
@@ -13,23 +13,23 @@ public class PointTest {
     }
     @Test
     public void testPoint1() {
-        Point p1 = new Point(1,3);
-        Point p2 = new Point(2,4);
-       Assert.assertEquals( p1.distance(p2), 1.41);
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(0,0);
+       Assert.assertEquals( p1.distance(p2), 0.0);
     }
 
     @Test
     public void testPoint2() {
         Point p1 = new Point(1,3);
         Point p2 = new Point(2,4);
-        Assert.assertEquals( p1.distance(p2), 0);
+        Assert.assertNotEquals( p1.distance(p2), -50);
     }
 
     @Test
     public void testPoint3() {
         Point p1 = new Point(1,3);
         Point p2 = new Point(2,4);
-        Assert.assertEquals( p1.distance(p2), -1.4142135623730951);
+        Assert.assertNotEquals( p1.distance(p2), -1.4142135623730951);
     }
 
 }
