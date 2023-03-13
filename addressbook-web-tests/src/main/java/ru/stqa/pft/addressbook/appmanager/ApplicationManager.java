47835737baaf2e0a0dedd28d581f3c4a5093a771
Sedinina <sedinina.k@yandex.ru>
2023-03-13 23:57:48 +0500

    Запускаем тесты в разных браузерах

--- addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,20 +2,37 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-        public WebDriver wd;
+    private String browser;
+    public WebDriver wd;
     private GroupHelper groupHelper;
     private NavigationHelper navigationHelper;
     private SessionHelper sessionHelper;
-    private  ContactHelper contactHelper;
+    private ContactHelper contactHelper;
+
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser.equals(Browser.FIREFOX.browserName())) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(Browser.CHROME.browserName())) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(Browser.IE.browserName())) {
+            wd = new InternetExplorerDriver();
+        }
+
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
@@ -32,7 +49,6 @@ public class ApplicationManager {
     public void exit() {
         wd.findElement(By.linkText("Logout")).click();
     }
-
 
 
     public GroupHelper getGroupHelper() {
