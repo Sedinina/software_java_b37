59d7ca5c3afb6d3cc053ec2944df4e40119b5608
Sedinina <sedinina.k@yandex.ru>
2023-03-14 10:06:45 +0500

    Реализовано проверка и выполнение предусловий для group

--- addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -1,18 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupDate;
 
 
 public class GroupDeletionTest extends TestBase {
 
-  @Test
-  public void testGroupDeletion()  {
-    app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selectGroup();
-    app.getGroupHelper().deleteSelectedGroups();
-    app.getGroupHelper().returnToGroupPage();
-    app.exit();
-  }
+    @Test
+    public void testGroupDeletion() {
+        app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupDate("test11", null, null));
+        }
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnToGroupPage();
+        app.exit();
+    }
 
 
 }
